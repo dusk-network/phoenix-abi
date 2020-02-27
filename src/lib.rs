eb221edfdaf65dcf3ac219498d48fc461b6894f8
Jules de Smit <julesdesmit@gmail.com>
2020-02-27 15:57:50 +0100

    block off all 'std' functionality to enable using phoenix_abi in a no_std environment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 mod types;
-use phoenix::MAX_NOTES_PER_TRANSACTION;
-pub use types::{Item, ITEM_SIZE, PROOF_SIZE};
+pub use types::{Item, ITEM_SIZE, MAX_NOTES_PER_TRANSACTION, PROOF_SIZE};
 
 mod external {
     use super::*;
--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,4 @@
 // pub use plonk_abi::Proof;
-use phoenix::{
-    CompressedRistretto, Nonce, NoteUtxoType, NoteVariant, Nullifier, ObfuscatedNote,
-    RistrettoPoint, Scalar, TransactionItem, TransparentNote,
-};
 use serde::de::Visitor;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -10,6 +6,8 @@ pub const ITEM_SIZE: usize = 305; // See `Item`
 
 // TODO: this should come from `plonk_abi`
 pub const PROOF_SIZE: usize = 600;
+
+pub const MAX_NOTES_PER_TRANSACTION: usize = 10;
 
 #[derive(Clone, Copy)]
 struct RistrettoPointBytes([u8; 64]);
@@ -137,58 +135,75 @@ pub struct Item {
     nullifier: [u8; 32],
 }
 
-impl From<Item> for TransactionItem {
-    fn from(item: Item) -> TransactionItem {
-        let mut tx_item = TransactionItem::default();
-        tx_item.set_note(item.into());
-
-        if tx_item.utxo() == NoteUtxoType::Input {
-            let nullifier = Nullifier::new(Scalar::from_canonical_bytes(item.nullifier).unwrap());
-            tx_item.set_nullifier(nullifier);
-        }
-        tx_item
+impl core::fmt::Debug for Item {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "a")
     }
 }
 
-impl From<Item> for NoteVariant {
-    fn from(item: Item) -> Self {
-        // We assume that the `utxo` field is encoded as either a 1
-        // or a 2
-        let utxo = match item.utxo {
-            1 => NoteUtxoType::Input,
-            2 => NoteUtxoType::Output,
-            _ => panic!("oh bother"), // NoteUtxoType::Unknown,
-        };
+#[cfg(feature = "std")]
+mod convert {
+    use super::Item;
 
-        let r_g = RistrettoPoint::from_uniform_bytes(&item.r_g.0);
-        let pk_r = RistrettoPoint::from_uniform_bytes(&item.pk_r.0);
-        let commitment = CompressedRistretto::from_slice(&item.commitment);
-        let nonce = Nonce::from_slice(&item.nonce).unwrap();
+    use phoenix::{
+        CompressedRistretto, Nonce, NoteUtxoType, NoteVariant, Nullifier, ObfuscatedNote,
+        RistrettoPoint, Scalar, TransactionItem, TransparentNote,
+    };
 
-        if item.value == 0 {
-            ObfuscatedNote::new(
-                utxo,
-                commitment,
-                nonce,
-                r_g,
-                pk_r,
-                item.idx.into(),
-                item.encrypted_value,
-                item.encrypted_blinding_factor.0,
-            )
-            .into()
-        } else {
-            TransparentNote::new(
-                utxo,
-                item.value,
-                nonce,
-                r_g,
-                pk_r,
-                item.idx.into(),
-                commitment,
-                item.encrypted_blinding_factor.0,
-            )
-            .into()
+    impl From<Item> for TransactionItem {
+        fn from(item: Item) -> TransactionItem {
+            let mut tx_item = TransactionItem::default();
+            tx_item.set_note(item.into());
+
+            if tx_item.utxo() == NoteUtxoType::Input {
+                let nullifier =
+                    Nullifier::new(Scalar::from_canonical_bytes(item.nullifier).unwrap());
+                tx_item.set_nullifier(nullifier);
+            }
+            tx_item
+        }
+    }
+
+    impl From<Item> for NoteVariant {
+        fn from(item: Item) -> Self {
+            // We assume that the `utxo` field is encoded as either a 1
+            // or a 2
+            let utxo = match item.utxo {
+                1 => NoteUtxoType::Input,
+                2 => NoteUtxoType::Output,
+                _ => panic!("oh bother"), // NoteUtxoType::Unknown,
+            };
+
+            let r_g = RistrettoPoint::from_uniform_bytes(&item.r_g.0);
+            let pk_r = RistrettoPoint::from_uniform_bytes(&item.pk_r.0);
+            let commitment = CompressedRistretto::from_slice(&item.commitment);
+            let nonce = Nonce::from_slice(&item.nonce).unwrap();
+
+            if item.value == 0 {
+                ObfuscatedNote::new(
+                    utxo,
+                    commitment,
+                    nonce,
+                    r_g,
+                    pk_r,
+                    item.idx.into(),
+                    item.encrypted_value,
+                    item.encrypted_blinding_factor.0,
+                )
+                .into()
+            } else {
+                TransparentNote::new(
+                    utxo,
+                    item.value,
+                    nonce,
+                    r_g,
+                    pk_r,
+                    item.idx.into(),
+                    commitment,
+                    item.encrypted_blinding_factor.0,
+                )
+                .into()
+            }
         }
     }
 }
