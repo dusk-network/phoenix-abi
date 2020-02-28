325dd794f45359c2b5008288c9501df19eedfee0
Jules de Smit <julesdesmit@gmail.com>
2020-02-28 15:32:04 +0100

    update FFI and 'store' signatures to exclude the proof
    this will be added back later on, as it is just a measure to allow for easy testing

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,11 @@ pub use types::{Item, ITEM_SIZE, MAX_NOTES_PER_TRANSACTION, PROOF_SIZE};
 mod external {
     use super::*;
     extern "C" {
-        pub fn phoenix_store(
-            items_buf: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE],
-            proof_buf: &[u8; PROOF_SIZE],
-        ) -> bool;
+        pub fn phoenix_store(items_buf: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]) -> bool;
     }
 }
 
 // TODO: fix proof
-pub fn store(items: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE], proof: u8) -> bool {
-    unsafe { external::phoenix_store(&items, &[0u8; PROOF_SIZE]) }
+pub fn store(items: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]) -> bool {
+    unsafe { external::phoenix_store(&items) }
 }
