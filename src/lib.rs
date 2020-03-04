00bc93d5f86c3017538bc7416399850900f3a076
Jules de Smit <julesdesmit@gmail.com>
2020-03-04 16:40:53 +0100

    add FFI for phoenix verification function

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,16 @@ mod external {
     use super::*;
     extern "C" {
         pub fn phoenix_store(items_buf: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]) -> bool;
+
+        pub fn phoenix_verify(items_buf: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]);
     }
 }
 
 // TODO: fix proof
 pub fn store(items: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]) -> bool {
     unsafe { external::phoenix_store(&items) }
+}
+
+pub fn verify(items: &[u8; MAX_NOTES_PER_TRANSACTION * ITEM_SIZE]) {
+    unsafe { external::phoenix_verify(&items) }
 }
