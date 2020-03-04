6270d9754cb9019219a64525257536149cf35569
Jules de Smit <julesdesmit@gmail.com>
2020-03-04 16:06:04 +0100

    replace serde implementations for macro

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 // pub use plonk_abi::Proof;
-use serde::de::Visitor;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use dusk_abi::impl_serde_for_array;
+use serde::{de::Visitor, ser::SerializeTuple, Deserialize, Deserializer, Serialize, Serializer};
 
 pub const ITEM_SIZE: usize = 305; // See `Item`
 
@@ -18,52 +18,7 @@ impl Default for RistrettoPointBytes {
     }
 }
 
-impl Serialize for RistrettoPointBytes {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        use serde::ser::SerializeTuple;
-        let mut seq = serializer.serialize_tuple(self.0.len())?;
-        for byte in self.0.iter() {
-            seq.serialize_element(byte)?;
-        }
-        seq.end()
-    }
-}
-
-impl<'de> Deserialize<'de> for RistrettoPointBytes {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct RistrettoPointBytesVisitor;
-
-        impl<'de> Visitor<'de> for RistrettoPointBytesVisitor {
-            type Value = RistrettoPointBytes;
-
-            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-                formatter.write_str("64 bytes")
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<RistrettoPointBytes, A::Error>
-            where
-                A: serde::de::SeqAccess<'de>,
-            {
-                let mut bytes = [0u8; 64];
-                for i in 0..64 {
-                    bytes[i] = seq
-                        .next_element()?
-                        .ok_or(serde::de::Error::invalid_length(i, &"expected 64 bytes"))?;
-                }
-
-                Ok(RistrettoPointBytes(bytes))
-            }
-        }
-
-        deserializer.deserialize_tuple(64, RistrettoPointBytesVisitor)
-    }
-}
+impl_serde_for_array!(RistrettoPointBytes, 64);
 
 impl From<[u8; 64]> for RistrettoPointBytes {
     fn from(arr: [u8; 64]) -> Self {
@@ -80,52 +35,7 @@ impl Default for BlindingFactorBytes {
     }
 }
 
-impl Serialize for BlindingFactorBytes {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        use serde::ser::SerializeTuple;
-        let mut seq = serializer.serialize_tuple(self.0.len())?;
-        for byte in self.0.iter() {
-            seq.serialize_element(byte)?;
-        }
-        seq.end()
-    }
-}
-
-impl<'de> Deserialize<'de> for BlindingFactorBytes {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct BlindingFactorBytesVisitor;
-
-        impl<'de> Visitor<'de> for BlindingFactorBytesVisitor {
-            type Value = BlindingFactorBytes;
-
-            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-                formatter.write_str("48 bytes")
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<BlindingFactorBytes, A::Error>
-            where
-                A: serde::de::SeqAccess<'de>,
-            {
-                let mut bytes = [0u8; 48];
-                for i in 0..48 {
-                    bytes[i] = seq
-                        .next_element()?
-                        .ok_or(serde::de::Error::invalid_length(i, &"expected 48 bytes"))?;
-                }
-
-                Ok(BlindingFactorBytes(bytes))
-            }
-        }
-
-        deserializer.deserialize_tuple(48, BlindingFactorBytesVisitor)
-    }
-}
+impl_serde_for_array!(BlindingFactorBytes, 48);
 
 impl From<[u8; 48]> for BlindingFactorBytes {
     fn from(arr: [u8; 48]) -> Self {
@@ -219,12 +129,3 @@ mod convert {
         }
     }
 }
-
-// pub struct EncodedNote([u8; ITEM_SIZE]);
-//
-// // TODO: impl serde
-// impl EncodedNote {
-//     pub fn as_array(&self) -> [u8; ITEM_SIZE] {
-//         self.0
-//     }
-// }
