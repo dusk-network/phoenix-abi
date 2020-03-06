21df8d1d90ee168624932dd7981c1ad9ba416c2b
Jules de Smit <julesdesmit@gmail.com>
2020-03-06 15:43:44 +0100

    Substitute arrays for type aliases in function signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 mod types;
 pub use types::{
-    Note, Nullifier, MAX_NOTES_PER_TRANSACTION, MAX_NULLIFIERS_PER_TRANSACTION, NOTE_SIZE,
-    NULLIFIER_SIZE, PROOF_SIZE,
+    Note, NotesBuffer, Nullifier, NullifiersBuffer, MAX_NOTES_PER_TRANSACTION,
+    MAX_NULLIFIERS_PER_TRANSACTION, NOTE_SIZE, NULLIFIER_SIZE, PROOF_SIZE,
 };
 
 mod external {
     use super::*;
     extern "C" {
-        pub fn phoenix_store(
-            nullifiers: &[u8; MAX_NULLIFIERS_PER_TRANSACTION * NULLIFIER_SIZE],
-            notes: &[u8; MAX_NOTES_PER_TRANSACTION * NOTE_SIZE],
-        ) -> bool;
+        pub fn phoenix_store(nullifiers: &NullifiersBuffer, notes: &NotesBuffer) -> bool;
     }
 }
 
 // TODO: fix proof
-pub fn store(
-    nullifiers: &[u8; MAX_NULLIFIERS_PER_TRANSACTION * NULLIFIER_SIZE],
-    notes: &[u8; MAX_NOTES_PER_TRANSACTION * NOTE_SIZE],
-) -> bool {
+pub fn store(nullifiers: &NullifiersBuffer, notes: &NotesBuffer) -> bool {
     unsafe { external::phoenix_store(&nullifiers, &notes) }
 }
