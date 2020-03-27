7328613bd5a7183dc70909b81baf88799987fd78
Jules de Smit <julesdesmit@gmail.com>
2020-03-27 12:23:16 +0100

    Add FFI for phoenix_credit

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 pub mod types;
 
-pub use types::{Note, Nullifier};
+pub use types::{Note, Nullifier, PublicKey};
 
 mod external {
     extern "C" {
         pub fn phoenix_store(nullifiers: *const u8, notes: *const u8) -> bool;
 
         pub fn phoenix_verify(nullifiers: *const u8, notes: *const u8) -> bool;
+
+        pub fn phoenix_credit(value: i32, pk: *const u8) -> bool;
     }
 }
 
@@ -36,4 +38,11 @@ pub fn verify(
     unsafe {
         external::phoenix_verify(nullifiers_buf.as_ptr(), notes_buf.as_ptr())
     }
+}
+
+pub fn credit(
+    value: u64,
+    pk: &PublicKey,
+) -> bool {
+    unsafe { external::phoenix_credit(value as i32, pk.as_bytes().as_ptr()) }
 }
